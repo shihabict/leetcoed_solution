c5acaf78bc5dc7a969a7be6deba1b193d5c904a5
farhanshihab321@gmail.com <39627966+Farhashihab@users.noreply.github.com>
2022-05-14 10:16:31 +0600

    Added palindrome

--- palindrome.py
+++ palindrome.py
@@ -1,9 +1,19 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
+        # if x < 0:
+        #     return 'false'
+        # else:
+        #     return str(x) == str(x)[::-1]
         if x < 0:
-            return 'false'
-        else:
-            return str(x) == str(x)[::-1]
+            return False
+
+        input_num = x
+        num = 0
+        while x > 0:
+            num = num * 10 + x % 10
+            x = x // 10
+        return num == input_num
 
 sol = Solution()
-print(sol.isPalindrome(-121))
+print(sol.isPalindrome(121))
+
